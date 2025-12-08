b1e4aaaeb299dc7eafa30584c31fe2cc030c9e68
tranducson511-create <tranducson511@gmail.com>
2025-12-08 09:32:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const VERIFY_TOKEN = "ongchu123";
 
-// Route Facebook webhook
 app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
 
-  if (mode === "subscribe" && token === VERIFY_TOKEN) {
-    console.log("WEBHOOK VERIFIED!");
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
-  }
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        console.log("WEBHOOK VERIFIED!");
+        res.status(200).send(challenge);
+    } else {
+        res.sendStatus(403);
+    }
 });
 
-// Server chạy trên Render
+app.post("/webhook", (req, res) => {
+    console.log("EVENT RECEIVED:", req.body);
+    res.sendStatus(200);
+});
+
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("Server is running on port " + PORT));
